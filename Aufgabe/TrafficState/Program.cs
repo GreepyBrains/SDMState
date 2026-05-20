97639ac5dd24bb3cc9886fd90607a14639d70a76
GreepyBrains <greepybrains@gmail.com>
2026-05-20 08:47:57 +0200

    init commit, who's this?

---
+++ Aufgabe/TrafficState/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        TrafficLight light = new TrafficLight();
+
+        while (true)
+        {
+            light.Display();
+            Console.WriteLine("Drücke eine Taste um die Ampel zu schalten.");
+            Console.ReadKey();
+            Console.Clear();
+            light.Next();
+        }
+    }
+}
+
+class TrafficLight
+{
+    private string _state = "red";
+
+    public void Display()
+    {
+        if (_state == "red")
+            Console.WriteLine("[ ] [ ] [R]\nSTOP");
+        else if (_state == "yellow")
+            Console.WriteLine("[ ] [Y] [ ]\nGET READY");
+        else if (_state == "green")
+            Console.WriteLine("[G] [ ] [ ]\nGO");
+    }
+
+    public void Next()
+    {
+        if (_state == "red")
+            _state = "green";
+        else if (_state == "yellow")
+            _state = "red";
+        else if (_state == "green")
+            _state = "yellow";
+    }
+}
---
+++ AufgabeEinfach/AufgabeEinfach/Program.cs
@@ -0,0 +1,75 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        TrafficLight light = new TrafficLight();
+        while (true)
+        {
+            light.Display();
+            Console.WriteLine("Drücke eine Taste um die Ampel zu schalten.");
+            Console.ReadKey();
+            Console.Clear();
+            light.Next();
+        }
+    }
+}
+
+abstract class TrafficLightState
+{
+    public abstract void Display();
+    public abstract TrafficLightState Next();
+}
+
+class RedState : TrafficLightState
+{
+    public override void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override TrafficLightState Next()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class YellowState : TrafficLightState
+{
+    public override void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override TrafficLightState Next()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class GreenState : TrafficLightState
+{
+    public override void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override TrafficLightState Next()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class TrafficLight
+{
+    private TrafficLightState _state = new RedState();
+
+    public void Display()
+    {
+        _state.Display();
+    }
+
+    public void Next()
+    {
+        _state = _state.Next();
+    }
+}
